8de4b6fe5f078f066f3f0565dec8fc3aadc53bf1
fwh <15279279713@163.com>
2021-06-10 14:02:25 +0800

    閰嶇疆craco

---
+++ craco.config.js
@@ -0,0 +1,30 @@
+const CracoLessPlugin = require('craco-less')
+const path = require('path')
+const resolve = pathUrl => path.join(__dirname, pathUrl)
+module.exports = {
+    webpack: {
+        alias: {
+            '@': resolve('src')
+        }
+    },
+    babel: {
+        plugins: [
+            ['import', {libraryName: 'antd', style: true}],
+            ['@babel/plugin-proposal-decorators', {legacy: true}]
+        ]
+    },
+    plugins: [
+        {
+            plugin: CracoLessPlugin,
+            options: {
+                // 此处根据 less-loader 版本的不同会有不同的配置，详见 less-loader 官方文档
+                lessLoaderOptions: {
+                    lessOptions: {
+                        modifyVars: {},
+                        javascriptEnabled: true
+                    }
+                }
+            }
+        }
+    ]
+}
